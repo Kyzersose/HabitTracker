e3e73234177553183b3922ab88078a7c3962c2be
KyzerSose <matt@theshaeffers.com>
2016-11-21 14:36:41 -0600

    Initial commit

---
+++ app/src/main/java/com/theshaeffers/habittracker/data/HabitContract.java
@@ -0,0 +1,33 @@
+package com.theshaeffers.habittracker.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by shaefferm on 11/21/2016.
+ */
+
+public final class HabitContract {
+
+    //Prevent accidental instantiation
+    private HabitContract() {}
+
+    //Inner class, defines habits table
+    public static final class Habits implements BaseColumns{
+
+        //Table name
+        public static final String TABLE_NAME = "habits";
+
+        //Habits table, column names
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_HABIT_NAME = "Habit";
+        public static final String COLUMN_HABIT_DAY = "Day";
+        public static final String COLUMN_HABIT_STATUS = "Status";
+
+        //Options for Status
+        public final static int STATUS_SCHEDULED = 0;
+        public final static int STATUS_INCOMPLETE = 1;
+        public final static int STAUS_COMPLETE = 2;
+
+}
+
+}
---
+++ app/src/main/java/com/theshaeffers/habittracker/data/HabitDbHelper.java
@@ -0,0 +1,23 @@
+package com.theshaeffers.habittracker.data;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.theshaeffers.habittracker.data.HabitContract.Habits;
+
+/**
+ * Created by shaefferm on 11/21/2016.
+ */
+
+public class HabitDbHelper extends SQLiteOpenHelper {
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
